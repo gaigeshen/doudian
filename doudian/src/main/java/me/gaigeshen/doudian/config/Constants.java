c640511a2b2030634fca42d77ab8a144dc2483d6
gaigeshen <gaigeshen@qq.com>
2020-11-26 10:11:45 +0800

    增强访问令牌帮助类，现在可以返回访问令牌相关的链接地址

--- doudian/src/main/java/me/gaigeshen/doudian/config/Constants.java
+++ doudian/src/main/java/me/gaigeshen/doudian/config/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
   public static final String API_URL = "https://openapi-fxg.jinritemai.com";
 
   /** 店铺授权地址 */
-  public static final String SHOP_AUTHORIZE_TEMPLATE_URL = "https://fxg.jinritemai.com/index.html#/ffa/open/applicationAuthorize?response_type=code&app_id=%s&redirect_uri=%s&state=%s";
+  public static final String AUTHORIZE_TEMPLATE_URL = "https://fxg.jinritemai.com/index.html#/ffa/open/applicationAuthorize?response_type=code&app_id=%s&redirect_uri=%s&state=%s";
 
   /** 访问令牌地址 */
   public static final String ACCESS_TOKEN_TEMPLATE_URL = "https://openapi-fxg.jinritemai.com/oauth2/access_token?app_id=%s&app_secret=%s&code=%s&grant_type=authorization_code";
