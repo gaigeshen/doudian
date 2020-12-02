efcc0ba3db02b04d9f5721b12fd9e3d9145f30fd
gaigeshen <gaigeshen@qq.com>
2020-12-02 14:53:27 +0800

    应用授权地址变更

--- doudian/src/main/java/me/gaigeshen/doudian/config/Constants.java
+++ doudian/src/main/java/me/gaigeshen/doudian/config/Constants.java
@@ -7,11 +7,11 @@ package me.gaigeshen.doudian.config;
  */
 public class Constants {
 
-  /** 接口请求地址 */
+  /** 远程服务请求地址 */
   public static final String API_URL = "https://openapi-fxg.jinritemai.com";
 
-  /** 店铺授权地址 */
-  public static final String AUTHORIZE_TEMPLATE_URL = "https://fxg.jinritemai.com/index.html#/ffa/open/applicationAuthorize?response_type=code&app_id=%s&redirect_uri=%s&state=%s";
+  /** 应用授权地址 */
+  public static final String AUTHORIZE_TEMPLATE_URL = "https://fuwu.jinritemai.com/authorize?service_id=%s&state=%s";
 
   /** 访问令牌地址 */
   public static final String ACCESS_TOKEN_TEMPLATE_URL = "https://openapi-fxg.jinritemai.com/oauth2/access_token?app_id=%s&app_secret=%s&code=%s&grant_type=authorization_code";
