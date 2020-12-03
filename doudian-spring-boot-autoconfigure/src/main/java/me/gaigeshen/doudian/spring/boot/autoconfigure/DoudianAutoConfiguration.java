2b554031d8dfe15b95c7fa5e7439c4315cb1a212
gaigeshen <gaigeshen@qq.com>
2020-12-03 10:20:50 +0800

    增加两个自动配置相关的项目模块

---
+++ doudian-spring-boot-autoconfigure/src/main/java/me/gaigeshen/doudian/spring/boot/autoconfigure/DoudianAutoConfiguration.java
@@ -0,0 +1,10 @@
+package me.gaigeshen.doudian.spring.boot.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author gaigeshen
+ */
+@Configuration
+public class DoudianAutoConfiguration {
+}
