a5fe4c2a2e9b3980eb2a40d091cf1d6d1117e0fd
gaigeshen <gaigeshen@qq.com>
2020-12-09 17:42:20 +0800

    简化抖店请求参数转化过程

--- doudian/src/main/java/me/gaigeshen/doudian/client/DoudianClient.java
+++ doudian/src/main/java/me/gaigeshen/doudian/client/DoudianClient.java
@@ -3,11 +3,12 @@ package me.gaigeshen.doudian.client;
 import me.gaigeshen.doudian.request.RequestExecutor;
 
 /**
- * 抖店客户端
  *
  * @author gaigeshen
  */
 public interface DoudianClient extends RequestExecutor {
 
+  <D extends DoudianData> D executeForData(DoudianParams params, String shopId) throws DoudianClientException;
 
+  void init() throws DoudianClientException;
 }
